60419b4ed549aac03799e584a28e445e444cee17
Jaremy Creechley <creechley@gmail.com>
2026-02-17 12:49:28 -0700

    baseline

--- tests/tobjc_runtime.nim
+++ tests/tobjc_runtime.nim
@@ -6,6 +6,16 @@ proc initWithUTF8String*(
   o: NSString, str: cstring
 ): NSString {.objc: "initWithUTF8String:".}
 
+type DestroyProbeObject = object of NSObject
+
+var destroyProbeTriggered = false
+
+proc `=destroy`(o: var DestroyProbeObject) =
+  destroyProbeTriggered = true
+  var base = NSObject(value: o.value)
+  release(base)
+  o.value = base.value
+
 suite "objc runtime ownership fundamentals":
   test "alloc/init NSString roundtrip":
     var s = NSString.alloc().initWithUTF8String("This is a test!")
@@ -71,3 +81,10 @@ suite "objc runtime ownership fundamentals":
     check(afterBlock < duringCount)
     release(o)
     check(o == nil)
+
+  test "subclass destroy hook runs in block scope":
+    destroyProbeTriggered = false
+    block:
+      var o = cast[DestroyProbeObject](new(getClass("NSObject")))
+      check(o != nil)
+    check(destroyProbeTriggered)
