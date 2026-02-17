31f2a9ef1488126505a32bf391e21c52cd71fc48
Jaremy Creechley <creechley@gmail.com>
2026-02-17 11:57:56 -0700

    convert to unittests

--- tests/tobjc_runtime.nim
+++ tests/tobjc_runtime.nim
@@ -1,16 +1,16 @@
+import std/unittest
 import darwin/objc/runtime
 import darwin/foundation/nsgeometry
 
 type
-    NSNumber = ptr object of NSObject
-    NSValue = ptr object of NSObject
+  NSNumber = ptr object of NSObject
+  NSValue = ptr object of NSObject
 
 proc valueWithRect(n: typedesc[NSValue], d: NSRect): NSValue {.objc: "valueWithRect:".}
 proc valueWithPoint(n: typedesc[NSValue], d: NSPoint): NSValue {.objc: "valueWithPoint:".}
 proc rectValue(n: NSValue): NSRect {.objc.}
 proc pointValue(n: NSValue): NSPoint {.objc.}
 proc description(n: NSValue): NSString {.objc.}
-
 
 proc numberWithDouble(n: typedesc[NSNumber], d: float): NSNumber {.objc: "numberWithDouble:".}
 proc numberWithFloat(n: typedesc[NSNumber], d: cfloat): NSNumber {.objc: "numberWithFloat:".}
@@ -20,256 +20,257 @@ proc floatValue(n: NSNumber): cfloat {.objc: "floatValue".}
 
 proc UTF8String(n: NSString): cstring {.objc: "UTF8String".}
 
-
 proc alloc*[T](o: typedesc[T]): T {.objc: "alloc".}
 
 proc initWithUTF8String(o: NSString, str: cstring): NSString {.objc: "initWithUTF8String:".}
 
-let a = NSString.alloc().initWithUTF8String("This is a test!")
+suite "objc runtime":
+  test "NSNumber and NSString values":
+    let a = NSString.alloc().initWithUTF8String("This is a test!")
+    let n = NSNumber.numberWithDouble(123.456)
+    let nf = NSNumber.numberWithFloat(123.456)
 
-let n = NSNumber.numberWithDouble(123.456)
-let nf = NSNumber.numberWithFloat(123.456)
+    check(n.doubleValue > 123 and n.doubleValue < 124)
+    check(nf.floatValue > 123 and nf.floatValue < 124)
+    check($a.UTF8String == "This is a test!")
+    a.release()
 
-doAssert(n.doubleValue > 123 and n.doubleValue < 124)
-doAssert(nf.floatValue > 123 and nf.floatValue < 124)
-doAssert($a.UTF8String == "This is a test!")
-
-block:
+  test "NSValue rect roundtrip":
     let v = NSValue.valueWithRect(NSMakeRect(1, 2, 3, 4))
-    doAssert($v.description.UTF8String == "NSRect: {{1, 2}, {3, 4}}")
-    doAssert(v.rectValue == NSMakeRect(1, 2, 3, 4))
+    check($v.description.UTF8String == "NSRect: {{1, 2}, {3, 4}}")
+    check(v.rectValue == NSMakeRect(1, 2, 3, 4))
 
-block:
+  test "NSValue point roundtrip":
     let v = NSValue.valueWithPoint(NSMakePoint(1, 2))
-    doAssert($v.description.UTF8String == "NSPoint: {1, 2}")
-    doAssert(v.pointValue == NSMakePoint(1, 2))
+    check($v.description.UTF8String == "NSPoint: {1, 2}")
+    check(v.pointValue == NSMakePoint(1, 2))
 
-
-a.release()
-
-block:
+  test "basic runtime class and selector functions":
     let nsObjectClass = getClass("NSObject")
-    doAssert(nsObjectClass != nil)
+    check(nsObjectClass != nil)
 
-    doAssert(selector("init") == sel_registerName("init"))
+    check(selector("init") == sel_registerName("init"))
 
     let allocObj = init(cast[NSObject](alloc(nsObjectClass)))
-    doAssert(allocObj != nil)
+    check(allocObj != nil)
     allocObj.release()
 
     let newObj = cast[NSObject](new(nsObjectClass))
-    doAssert(newObj != nil)
+    check(newObj != nil)
     newObj.release()
 
-block:
+  test "addClass registers methods":
     const
-        BaseClassName = "NimRuntimeAddClassBase"
-        SubClassName = "NimRuntimeAddClassSub"
+      BaseClassName = "NimRuntimeAddClassBase"
 
     var
-        basePingCount = 0
-        subPingCount = 0
+      basePingCount = 0
 
     proc basePing(self: ID, cmd: SEL) {.cdecl.} =
-        inc(basePingCount)
+      inc(basePingCount)
 
     proc ping(self: NSObject) {.objc.}
 
     var baseCls: ObjcClass
     addClass(BaseClassName, "NSObject", baseCls):
-        addMethod("ping", basePing)
-    doAssert(baseCls != nil)
-    doAssert(getClass(BaseClassName) == baseCls)
+      addMethod("ping", basePing)
+    check(baseCls != nil)
+    check(getClass(BaseClassName) == baseCls)
 
-block:
+    let o = cast[NSObject](new(baseCls))
+    ping(o)
+    check(basePingCount == 1)
+    o.release()
+
+  test "callSuper with explicit return type":
     const
-        BaseClassName = "NimRuntimeCallSuperBase"
-        SubClassName = "NimRuntimeCallSuperSub"
+      BaseClassName = "NimRuntimeCallSuperBase"
+      SubClassName = "NimRuntimeCallSuperSub"
 
     var
-        baseSum = 0
-        subCallCount = 0
-        subReceived = 0
+      baseSum = 0
+      subCallCount = 0
+      subReceived = 0
 
     proc addBase(self: ID, cmd: SEL, value: cint): cint {.cdecl.} =
-        baseSum += value.int
-        baseSum.cint
+      baseSum += value.int
+      baseSum.cint
 
     proc addSub(self: ID, cmd: SEL, value: cint): cint {.cdecl.} =
-        inc(subCallCount)
-        subReceived = value.int
-        result = callSuper(cint, cast[NSObject](self), cmd, value)
-        inc(result)
+      inc(subCallCount)
+      subReceived = value.int
+      result = callSuper(cint, cast[NSObject](self), cmd, value)
+      inc(result)
 
     proc addValue(self: NSObject, value: cint): cint {.objc: "addValue:".}
 
     let baseCls = allocateClassPair(getClass("NSObject"), BaseClassName, 0)
     discard addMethod(baseCls, selector("addValue:"), addBase)
     registerClassPair(baseCls)
-    doAssert(baseCls != nil)
+    check(baseCls != nil)
 
     let subCls = allocateClassPair(baseCls, SubClassName, 0)
     discard addMethod(subCls, selector("addValue:"), addSub)
     registerClassPair(subCls)
-    doAssert(subCls != nil)
+    check(subCls != nil)
 
     let o = cast[NSObject](new(subCls))
     let actual = addValue(o, 10)
-    doAssert(actual == 11)
-    doAssert(baseSum == 10)
-    doAssert(subCallCount == 1)
-    doAssert(subReceived == 10)
+    check(actual == 11)
+    check(baseSum == 10)
+    check(subCallCount == 1)
+    check(subReceived == 10)
     o.release()
 
-block:
+  test "callSuper with implicit return type":
     const
-        BaseClassName = "NimRuntimeCallSuperImplicitRetBase"
-        SubClassName = "NimRuntimeCallSuperImplicitRetSub"
+      BaseClassName = "NimRuntimeCallSuperImplicitRetBase"
+      SubClassName = "NimRuntimeCallSuperImplicitRetSub"
 
     var
-        baseCallCount = 0
-        subCallCount = 0
-        subReceived: ID
+      baseCallCount = 0
+      subCallCount = 0
+      subReceived: ID
 
     proc identityBase(self: ID, cmd: SEL, value: ID): ID {.cdecl.} =
-        inc(baseCallCount)
-        value
+      inc(baseCallCount)
+      value
 
     proc identitySub(self: ID, cmd: SEL, value: ID): ID {.cdecl.} =
-        inc(subCallCount)
-        subReceived = value
-        result = callSuper(cast[NSObject](self), cmd, value)
+      inc(subCallCount)
+      subReceived = value
+      result = callSuper(cast[NSObject](self), cmd, value)
 
     proc identity(self: NSObject, value: NSObject): NSObject {.objc: "identity:".}
 
     let baseCls = allocateClassPair(getClass("NSObject"), BaseClassName, 0)
     discard addMethod(baseCls, selector("identity:"), identityBase)
     registerClassPair(baseCls)
-    doAssert(baseCls != nil)
+    check(baseCls != nil)
 
     let subCls = allocateClassPair(baseCls, SubClassName, 0)
     discard addMethod(subCls, selector("identity:"), identitySub)
     registerClassPair(subCls)
-    doAssert(subCls != nil)
+    check(subCls != nil)
 
     let o = cast[NSObject](new(subCls))
     let arg = cast[NSObject](new(getClass("NSObject")))
     let actual = identity(o, arg)
-    doAssert(actual == arg)
-    doAssert(subReceived == cast[ID](arg))
-    doAssert(baseCallCount == 1)
-    doAssert(subCallCount == 1)
+    check(actual == arg)
+    check(subReceived == cast[ID](arg))
+    check(baseCallCount == 1)
+    check(subCallCount == 1)
     arg.release()
     o.release()
 
-block:
+  test "callSuper no args with explicit return type":
     const
-        BaseClassName = "NimRuntimeCallSuperNoArgsBase"
-        SubClassName = "NimRuntimeCallSuperNoArgsSub"
+      BaseClassName = "NimRuntimeCallSuperNoArgsBase"
+      SubClassName = "NimRuntimeCallSuperNoArgsSub"
 
     var
-        baseCallCount = 0
-        subCallCount = 0
+      baseCallCount = 0
+      subCallCount = 0
 
     proc valueBase(self: ID, cmd: SEL): cint {.cdecl.} =
-        inc(baseCallCount)
-        41
+      inc(baseCallCount)
+      41
 
     proc valueSub(self: ID, cmd: SEL): cint {.cdecl.} =
-        inc(subCallCount)
-        result = callSuper(cint, cast[NSObject](self), cmd)
-        inc(result)
+      inc(subCallCount)
+      result = callSuper(cint, cast[NSObject](self), cmd)
+      inc(result)
 
     proc value(self: NSObject): cint {.objc: "value".}
 
     let baseCls = allocateClassPair(getClass("NSObject"), BaseClassName, 0)
     discard addMethod(baseCls, selector("value"), valueBase)
     registerClassPair(baseCls)
-    doAssert(baseCls != nil)
+    check(baseCls != nil)
 
     let subCls = allocateClassPair(baseCls, SubClassName, 0)
     discard addMethod(subCls, selector("value"), valueSub)
     registerClassPair(subCls)
-    doAssert(subCls != nil)
+    check(subCls != nil)
 
     let o = cast[NSObject](new(subCls))
     let actual = value(o)
-    doAssert(actual == 42)
-    doAssert(baseCallCount == 1)
-    doAssert(subCallCount == 1)
+    check(actual == 42)
+    check(baseCallCount == 1)
+    check(subCallCount == 1)
     o.release()
 
-block:
+  test "callSuper no args with implicit return type":
     const
-        BaseClassName = "NimRuntimeCallSuperNoArgsImplicitRetBase"
-        SubClassName = "NimRuntimeCallSuperNoArgsImplicitRetSub"
+      BaseClassName = "NimRuntimeCallSuperNoArgsImplicitRetBase"
+      SubClassName = "NimRuntimeCallSuperNoArgsImplicitRetSub"
 
     var
-        baseCallCount = 0
-        subCallCount = 0
+      baseCallCount = 0
+      subCallCount = 0
 
     proc selfObjectBase(self: ID, cmd: SEL): ID {.cdecl, varargs.} =
-        inc(baseCallCount)
-        self
+      inc(baseCallCount)
+      self
 
     proc selfObjectSub(self: ID, cmd: SEL): ID {.cdecl, varargs.} =
-        inc(subCallCount)
-        result = callSuper(cast[NSObject](self), cmd)
+      inc(subCallCount)
+      result = callSuper(cast[NSObject](self), cmd)
 
     proc selfObject(self: NSObject): NSObject {.objc: "selfObject".}
 
     let baseCls = allocateClassPair(getClass("NSObject"), BaseClassName, 0)
     discard addMethod(baseCls, selector("selfObject"), selfObjectBase)
     registerClassPair(baseCls)
-    doAssert(baseCls != nil)
+    check(baseCls != nil)
 
     let subCls = allocateClassPair(baseCls, SubClassName, 0)
     discard addMethod(subCls, selector("selfObject"), selfObjectSub)
     registerClassPair(subCls)
-    doAssert(subCls != nil)
+    check(subCls != nil)
 
     let o = cast[NSObject](new(subCls))
     let actual = selfObject(o)
-    doAssert(cast[ID](actual) == cast[ID](o))
-    doAssert(baseCallCount == 1)
-    doAssert(subCallCount == 1)
+    check(cast[ID](actual) == cast[ID](o))
+    check(baseCallCount == 1)
+    check(subCallCount == 1)
     o.release()
 
-when not defined(arm64):
-    block:
+  when not defined(arm64):
+    test "callSuper with NSRect return":
         const
-            BaseClassName = "NimRuntimeCallSuperStretBase"
-            SubClassName = "NimRuntimeCallSuperStretSub"
+          BaseClassName = "NimRuntimeCallSuperStretBase"
+          SubClassName = "NimRuntimeCallSuperStretSub"
 
         var
-            baseRectCount = 0
-            subRectCount = 0
+          baseRectCount = 0
+          subRectCount = 0
 
         proc testRectBase(self: ID, cmd: SEL): NSRect {.cdecl.} =
-            inc(baseRectCount)
-            NSMakeRect(1, 2, 30, 40)
+          inc(baseRectCount)
+          NSMakeRect(1, 2, 30, 40)
 
         proc testRectSub(self: ID, cmd: SEL): NSRect {.cdecl.} =
-            inc(subRectCount)
-            result = callSuper(NSRect, cast[NSObject](self), cmd)
-            result.origin.x += 10
-            result.size.width += 5
+          inc(subRectCount)
+          result = callSuper(NSRect, cast[NSObject](self), cmd)
+          result.origin.x += 10
+          result.size.width += 5
 
         proc testRect(self: NSObject): NSRect {.objc: "testRect".}
 
         var baseCls: ObjcClass
         addClass(BaseClassName, "NSObject", baseCls):
-            addMethod("testRect", testRectBase)
-        doAssert(baseCls != nil)
+          addMethod("testRect", testRectBase)
+        check(baseCls != nil)
 
         var subCls: ObjcClass
         addClass(SubClassName, BaseClassName, subCls):
-            addMethod("testRect", testRectSub)
-        doAssert(subCls != nil)
+          addMethod("testRect", testRectSub)
+        check(subCls != nil)
 
         let o = cast[NSObject](new(subCls))
         let r = testRect(o)
-        doAssert(r == NSMakeRect(11, 2, 35, 40))
-        doAssert(baseRectCount == 1)
-        doAssert(subRectCount == 1)
+        check(r == NSMakeRect(11, 2, 35, 40))
+        check(baseRectCount == 1)
+        check(subRectCount == 1)
         o.release()
