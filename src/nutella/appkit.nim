ffb09213e7e31f1a889de5231416b84a336a4765
Jaremy Creechley <creechley@gmail.com>
2026-02-22 22:22:01 -0700

    refactor appkit modules

--- src/nutella/appkit.nim
+++ src/nutella/appkit.nim
@@ -2,8 +2,10 @@ import ./appkit/runtime
 import ./appkit/rendering
 import ./appkit/application
 import ./appkit/views
+import ./appkit/windows
 
 export runtime
 export application
 export views
+export windows
 
