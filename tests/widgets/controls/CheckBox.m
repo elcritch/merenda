ffcc3eee360957904d8491da85e3cc6f11c17d9d
Jaremy Creechley <creechley@gmail.com>
2026-02-27 21:59:51 -0700

    implement more

--- tests/widgets/controls/CheckBox.m
+++ tests/widgets/controls/CheckBox.m
@@ -1,5 +1,7 @@
 #include <Cocoa/Cocoa.h>
 
+static const char* nsstr(NSString* s) { return s ? [s UTF8String] : ""; }
+
 @interface Window : NSWindow {
   NSButton* checkBox1;
   NSButton* checkBox2;
@@ -9,9 +11,60 @@
 }
 - (instancetype)init;
 - (BOOL)windowShouldClose:(id)sender;
+- (IBAction) OnCheckBox1Click:(id)sender;
+- (IBAction) OnCheckBox2Click:(id)sender;
+- (IBAction) OnCheckBox3Click:(id)sender;
+- (IBAction) OnCheckBox4Click:(id)sender;
+- (IBAction) OnCheckBox5Click:(id)sender;
+- (NSString*) stateToString:(NSControlStateValue)state;
+- (void)dumpLayout:(NSString*)stage;
 @end
 
 @implementation Window
+- (void)dumpView:(NSView*)view name:(NSString*)name {
+  NSRect frame = [view frame];
+  NSRect bounds = [view bounds];
+  NSLog(@"[%@] frame=(%.1f,%.1f %.1fx%.1f) bounds=(%.1f,%.1f %.1fx%.1f) autoresizeMask=0x%lx",
+      name,
+      frame.origin.x, frame.origin.y, frame.size.width, frame.size.height,
+      bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height,
+      (unsigned long)[view autoresizingMask]);
+  printf("[%s] frame=(%.1f,%.1f %.1fx%.1f) bounds=(%.1f,%.1f %.1fx%.1f) autoresizeMask=0x%lx\n",
+      nsstr(name),
+      frame.origin.x, frame.origin.y, frame.size.width, frame.size.height,
+      bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height,
+      (unsigned long)[view autoresizingMask]);
+}
+
+- (void)dumpLayout:(NSString*)stage {
+  NSRect windowFrame = [self frame];
+  NSRect contentRect = [self contentRectForFrameRect:windowFrame];
+  NSLog(@"[Window %@] frame=(%.1f,%.1f %.1fx%.1f) contentRect=(%.1f,%.1f %.1fx%.1f)",
+      stage,
+      windowFrame.origin.x, windowFrame.origin.y, windowFrame.size.width, windowFrame.size.height,
+      contentRect.origin.x, contentRect.origin.y, contentRect.size.width, contentRect.size.height);
+  printf("[Window %s] frame=(%.1f,%.1f %.1fx%.1f) contentRect=(%.1f,%.1f %.1fx%.1f)\n",
+      nsstr(stage),
+      windowFrame.origin.x, windowFrame.origin.y, windowFrame.size.width, windowFrame.size.height,
+      contentRect.origin.x, contentRect.origin.y, contentRect.size.width, contentRect.size.height);
+  [self dumpView:[self contentView] name:@"contentView"];
+  [self dumpView:checkBox1 name:@"checkBox1"];
+  [self dumpView:checkBox2 name:@"checkBox2"];
+  [self dumpView:checkBox3 name:@"checkBox3"];
+  [self dumpView:checkBox4 name:@"checkBox4"];
+  [self dumpView:checkBox5 name:@"checkBox5"];
+  NSLog(@"[checkBox1] state=%ld mixed=%d bezel=%ld alignment=%ld title='%@'",
+      (long)[checkBox1 state], [checkBox1 allowsMixedState], (long)[checkBox1 bezelStyle], (long)[checkBox1 alignment], [checkBox1 title]);
+  NSLog(@"[checkBox2] state=%ld mixed=%d bezel=%ld alignment=%ld title='%@'",
+      (long)[checkBox2 state], [checkBox2 allowsMixedState], (long)[checkBox2 bezelStyle], (long)[checkBox2 alignment], [checkBox2 title]);
+  NSLog(@"[checkBox3] state=%ld mixed=%d bezel=%ld alignment=%ld title='%@'",
+      (long)[checkBox3 state], [checkBox3 allowsMixedState], (long)[checkBox3 bezelStyle], (long)[checkBox3 alignment], [checkBox3 title]);
+  NSLog(@"[checkBox4] state=%ld mixed=%d bezel=%ld alignment=%ld title='%@'",
+      (long)[checkBox4 state], [checkBox4 allowsMixedState], (long)[checkBox4 bezelStyle], (long)[checkBox4 alignment], [checkBox4 title]);
+  NSLog(@"[checkBox5] state=%ld mixed=%d bezel=%ld alignment=%ld title='%@'",
+      (long)[checkBox5 state], [checkBox5 allowsMixedState], (long)[checkBox5 bezelStyle], (long)[checkBox5 alignment], [checkBox5 title]);
+}
+
 - (instancetype)init {
   checkBox1 = [[[NSButton alloc] initWithFrame:NSMakeRect(30, 250, 105, 20)] autorelease];
   [checkBox1 setTitle:@"Unchecked"];
@@ -64,6 +117,7 @@
   [[self contentView] addSubview:checkBox4];
   [[self contentView] addSubview:checkBox5];
   [self setIsVisible:YES];
+  [self dumpLayout:@"init"];
   return self;
 }
 
@@ -75,23 +129,28 @@
 - (IBAction) OnCheckBox1Click:(id)sender {
   [checkBox1 setState:NSControlStateValueOff];
   [checkBox1 setTitle: [self stateToString: [checkBox1 state]]];
+  [self dumpLayout:@"checkBox1-click"];
 }
 
 - (IBAction) OnCheckBox2Click:(id)sender {
   [checkBox2 setTitle: [self stateToString: [checkBox2 state]]];
+  [self dumpLayout:@"checkBox2-click"];
 }
 
 - (IBAction) OnCheckBox3Click:(id)sender {
   [checkBox3 setTitle: [self stateToString: [checkBox3 state]]];
+  [self dumpLayout:@"checkBox3-click"];
 }
 
 - (IBAction) OnCheckBox4Click:(id)sender {
   [checkBox4 setTitle: [self stateToString: [checkBox4 state]]];
+  [self dumpLayout:@"checkBox4-click"];
 }
 
 - (IBAction) OnCheckBox5Click:(id)sender {
   [checkBox5 setState:NSControlStateValueOff];
   [checkBox5 setTitle: [self stateToString: [checkBox5 state]]];
+  [self dumpLayout:@"checkBox5-click"];
 }
 
 - (NSString*) stateToString:(NSControlStateValue)state {
@@ -100,6 +159,7 @@
     case NSControlStateValueOn: return @"Checked";
     case NSControlStateValueMixed: return @"Mixed";
   }
+  return @"Unchecked";
 }
 @end
 
@@ -111,7 +171,9 @@ int main(int argc, char *argv[]) {
     [window makeKeyAndOrderFront:nil];
 
     [NSApp activateIgnoringOtherApps:YES];
+    if (getenv("CHECKBOX_DUMP_LAYOUT_ONCE") != NULL) {
+      [NSApp performSelector:@selector(terminate:) withObject:nil afterDelay:0.2];
+    }
     [NSApp run];
     return 0;
 }
-
--- tests/widgets/controls/Label.m
+++ tests/widgets/controls/Label.m
@@ -1,13 +1,48 @@
 #include <Cocoa/Cocoa.h>
 
+static const char* nsstr(NSString* s) { return s ? [s UTF8String] : ""; }
+
 @interface Window : NSWindow {
   NSTextField* label1;
 }
 - (instancetype)init;
 - (BOOL)windowShouldClose:(id)sender;
+- (void)dumpLayout:(NSString*)stage;
 @end
 
 @implementation Window
+- (void)dumpView:(NSView*)view name:(NSString*)name {
+  NSRect frame = [view frame];
+  NSRect bounds = [view bounds];
+  NSLog(@"[%@] frame=(%.1f,%.1f %.1fx%.1f) bounds=(%.1f,%.1f %.1fx%.1f) autoresizeMask=0x%lx",
+      name,
+      frame.origin.x, frame.origin.y, frame.size.width, frame.size.height,
+      bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height,
+      (unsigned long)[view autoresizingMask]);
+  printf("[%s] frame=(%.1f,%.1f %.1fx%.1f) bounds=(%.1f,%.1f %.1fx%.1f) autoresizeMask=0x%lx\n",
+      nsstr(name),
+      frame.origin.x, frame.origin.y, frame.size.width, frame.size.height,
+      bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height,
+      (unsigned long)[view autoresizingMask]);
+}
+
+- (void)dumpLayout:(NSString*)stage {
+  NSRect windowFrame = [self frame];
+  NSRect contentRect = [self contentRectForFrameRect:windowFrame];
+  NSLog(@"[Window %@] frame=(%.1f,%.1f %.1fx%.1f) contentRect=(%.1f,%.1f %.1fx%.1f)",
+      stage,
+      windowFrame.origin.x, windowFrame.origin.y, windowFrame.size.width, windowFrame.size.height,
+      contentRect.origin.x, contentRect.origin.y, contentRect.size.width, contentRect.size.height);
+  printf("[Window %s] frame=(%.1f,%.1f %.1fx%.1f) contentRect=(%.1f,%.1f %.1fx%.1f)\n",
+      nsstr(stage),
+      windowFrame.origin.x, windowFrame.origin.y, windowFrame.size.width, windowFrame.size.height,
+      contentRect.origin.x, contentRect.origin.y, contentRect.size.width, contentRect.size.height);
+  [self dumpView:[self contentView] name:@"contentView"];
+  [self dumpView:label1 name:@"label1"];
+  NSLog(@"[label1] string='%@' bezeled=%d drawsBackground=%d editable=%d selectable=%d alignment=%ld",
+      [label1 stringValue], [label1 isBezeled], [label1 drawsBackground], [label1 isEditable], [label1 isSelectable], (long)[label1 alignment]);
+}
+
 - (instancetype)init {
   label1 = [[[NSTextField alloc] initWithFrame:NSMakeRect(10, 270, 100, 20)] autorelease];
   [label1 setStringValue:@"label1"];
@@ -20,6 +55,7 @@
   [self setTitle:@"Label Example"];
   [[self contentView] addSubview:label1];
   [self setIsVisible:YES];
+  [self dumpLayout:@"init"];
   return self;
 }
 
@@ -37,7 +73,9 @@ int main(int argc, char *argv[]) {
     [window makeKeyAndOrderFront:nil];
 
     [NSApp activateIgnoringOtherApps:YES];
+    if (getenv("LABEL_DUMP_LAYOUT_ONCE") != NULL) {
+      [NSApp performSelector:@selector(terminate:) withObject:nil afterDelay:0.2];
+    }
     [NSApp run];
     return 0;
 }
-
