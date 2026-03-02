ee83d88db1e465088396555259c59e51588ad12a
Jaremy Creechley <creechley@gmail.com>
2026-03-02 02:25:54 -0700

    convert to enums

--- src/knutella/appkit/buttons.nim
+++ src/knutella/appkit/buttons.nim
@@ -10,180 +10,67 @@ type NSButtonCallbackProc = proc(sender: IDPtr)
 
 objcImpl:
   type NSButton* = object of NSControl
-    title {.set: setTitle.}: NSString
-    alternateTitle {.set: setAlternateTitle.}: NSString
-    keyEquivalent {.set: setKeyEquivalent.}: NSString
 
-    bordered {.get: isBordered.}: bool
-    bezeled {.get: isBezeled.}: bool
-    bezelStyle {.set: setBezelStyle.}: int # NSBezelStyle
+  method resignFirstResponder*(self: NSButton): bool =
+    self.setNeedsDisplay()
+    return callSuperAs[NSControl, bool](self, getSelector("resignFirstResponder"))
 
-    stateValue {.get: state.}: int
-    mixedAllowed {.get: allowsMixedState.}: bool
-    transparent {.set: setTransparent, get: isTransparent.}: bool
-    keyEquivalentModifierMask {.set: setKeyEquivalentModifierMask.}: int
-      # NSEventModifierFlags
-
-    imagePos {.set: setImagePosition, get: imagePosition.}: int
-
-    showBorderInside {.
-      set: setShowsBorderOnlyWhileMouseInside, get: showsBorderOnlyWhileMouseInside
-    .}: bool
-    xButtonType: cint
-    xHighlighted {.set: setHighlighted, get: isHighlighted.}: bool
-    xHighlightsBy {.set: setHighlightsBy, get: highlightsBy.}: int
-    xShowsStateBy {.set: setShowsStateBy, get: showsStateBy.}: int
-    periodicDelaySec: float32
-    periodicIntervalSec: float32
-    onClick: NSButtonCallbackProc
-
-  template syncButtonCellFromView(self: NSButton) =
-    if self.isNil:
-      return
-    let control = self.NSControl
-    var selected = control.cell()
-    if selected.isNil or (not selected.isKindOfClass(NSButtonCell)):
-      var created = NSButtonCell.new()
-      control.setCell(ownFromId[NSCell](created.value))
-      selected = ownFromId[NSCell](created.value)
-      created.value = nil
-    if selected.isNil:
-      return
-    let cell = selected.NSButtonCell
-    if cell.isNil:
-      return
-    cell.setControlView(self.NSView)
-    cell.setTitle(self.title)
-    cell.setAlternateTitle(self.alternateTitle)
-    cell.setKeyEquivalent(self.keyEquivalent)
-    cell.setButtonType(self.xButtonType)
-    cell.setBordered(self.bordered)
-    cell.setBezeled(self.bezeled)
-    cell.setBezelStyle(self.bezelStyle)
-    cell.setImagePosition(self.imagePos)
-    cell.setTransparent(self.transparent)
-    cell.setKeyEquivalentModifierMask(self.keyEquivalentModifierMask)
-    cell.setShowsBorderOnlyWhileMouseInside(self.showBorderInside)
-    cell.setHighlightsBy(self.xHighlightsBy)
-    cell.setShowsStateBy(self.xShowsStateBy)
-    cell.setAllowsMixedState(self.mixedAllowed)
-    cell.setState(self.stateValue)
-    cell.setHighlighted(self.xHighlighted)
-    cell.setPeriodicDelay(self.periodicDelaySec, interval = self.periodicIntervalSec)
-
-  method init*(self: var NSButton): NSButton =
-    result = asTypeRaw[NSButton](callSuperIdFrom(NSButton, self, getSelector("init")))
-    if result.isNil:
-      return
-    result.setEnabled(true)
-    result.setAlignment(NSCenterTextAlignment)
-    result.title = @ns"Button"
-    result.stateValue = NSOffState
-    result.mixedAllowed = false
-    result.bordered = true
-    result.bezeled = true
-    result.transparent = false
-    result.keyEquivalent = @ns""
-    result.keyEquivalentModifierMask = 0
-    result.imagePos = 0
-    result.bezelStyle = 0
-    result.alternateTitle = @ns""
-    result.showBorderInside = false
-    result.xButtonType = NSMomentaryPushInButton.cint
-    result.xHighlighted = false
-    result.xHighlightsBy = NSPushInCellMask
-    result.xShowsStateBy = NSNoCellMask
-    result.periodicDelaySec = 0.0
-    result.periodicIntervalSec = 0.0
-    result.onClick = nil
-    var buttonCell = NSButtonCell.new()
-    #asType[NSControl](result).setCell(ownFromId[NSCell](buttonCell.value))
-    result.NSControl.setCell(ownFromId[NSCell](buttonCell.value))
-    buttonCell.value = nil
-    result.syncButtonCellFromView()
-
-  method initWithFrame*(
-      self: var NSButton,
-      x: float32,
-      y {.kw("y").}: float32,
-      width {.kw("width").}: float32,
-      height {.kw("height").}: float32,
-  ): NSButton =
-    result = self.init()
-    if result.isNil:
-      return
-    result.to(NSView).setFrame(
-      x.float32, y.float32, max(width.float32, 0.0), max(height.float32, 0.0)
-    )
-
-  method setState*(self: NSButton, value: cint) =
-    self.stateValue = normalizeButtonState(value.int, self.mixedAllowed)
-    self.syncButtonCellFromView()
+  method setTitle*(self: NSButton, value: NSString) =
+    self.setTitle(value)
     self.setNeedsDisplay(true)
 
-  method setAllowsMixedState*(self: NSButton, value: bool) =
-    self.mixedAllowed = value
-    self.stateValue = normalizeButtonState(self.stateValue, value)
-    self.syncButtonCellFromView()
+  method setState*(self: NSButton, value: int) =
+    self.xCell.setState(value)
     self.setNeedsDisplay(true)
 
   method setNextState*(self: NSButton) =
-    if self.mixedAllowed:
-      case self.stateValue
-      of NSOffState:
-        self.stateValue = NSOnState
-      of NSOnState:
-        self.stateValue = NSMixedState
-      else:
-        self.stateValue = NSOffState
-    else:
-      if self.stateValue == NSOnState:
-        self.stateValue = NSOffState
-      else:
-        self.stateValue = NSOnState
-    self.syncButtonCellFromView()
+    self.xCell.setNextState()
     self.setNeedsDisplay(true)
 
-  method stringValue*(self: NSButton): NSString =
-    self.title()
+  method setAllowsMixedState*(self: NSButton, value: bool) =
+    self.xCell.setAllowsMixedState()
 
-  method setStringValue*(self: NSButton, value: NSString) =
-    self.setTitle(value)
+  method setBezelStyle*(self: NSButton, value: NSBezelStyle) =
+    self.xCell.setBezelStyle(value)
+    self.setNeedsDisplay(true)
 
-  method intValue*(self: NSButton): cint =
-    self.state().cint
+  method setAlternateTitle*(self: NSButton, value: NSString) =
+    self.xCell.setAlternateTitle(value)
+    self.setNeedsDisplay(true)
 
-  method integerValue*(self: NSButton): int =
-    self.state()
+  method setAlternateImage*(self: NSButton, value: NSString) =
+    self.xCell.setAlternateImage(value)
+    self.setNeedsDisplay(true)
 
-  method floatValue*(self: NSButton): float32 =
-    self.state().float32
+  method setAttributedTitle*(self: NSButton, value: NSAttributedString) =
+    self.xCell.setAttributedTitle(value)
+    self.setNeedsDisplay(true)
 
-  method doubleValue*(self: NSButton): float =
-    self.state().float
-
-  method setIntValue*(self: NSButton, value: cint) =
-    self.setState(value)
-
-  method setIntegerValue*(self: NSButton, value: int) =
-    self.setState(value.cint)
-
-  method setFloatValue*(self: NSButton, value: float32) =
-    self.setState(value.int.cint)
-
-  method setDoubleValue*(self: NSButton, value: float) =
-    self.setState(value.int.cint)
+  method setAttributedAlternateTitle*(self: NSButton, value: NSAttributedString) =
+    self.xCell.setAttributedAlternateTitle(value)
+    self.setNeedsDisplay(true)
 
   method performClick*(self: NSButton, sender: NSResponder) =
-    if not self.isEnabled():
-      return
-    self.setNextState()
-    let control = self.NSControl
-    discard control.sendAction(control.action(), control.target())
-    let cb = self.onClick()
-    if cb.isNil:
-      return
-    cb(self.value)
+    self.highlight(true)
+    self.xCell.setState(self.xCell.nextState())
+    self.sendAction(self.action, to = self.target())
+    self.highlight(false)
+
+    #if not self.isEnabled():
+    #  return
+    #self.setNextState()
+    #let control = self.NSControl
+    #discard control.sendAction(control.action(), control.target())
+    #let cb = self.onClick()
+    #if cb.isNil:
+    #  return
+    #cb(self.value)
+
+  method keyDown*(self: NSButton, event: NSEvent) =
+    if event.charactersIgnoringModifiers().isEqualToString(@ns" "):
+      self.performClick(nil)
+    else:
+      self.interpretKeyEvents(@ns[event])
 
   method setPeriodicDelay*(
       self: NSButton, delay: float32, interval {.kw("interval").}: float32
@@ -238,20 +125,23 @@ objcImpl:
     self.syncButtonCellFromView()
     self.setNeedsDisplay(true)
 
-  method drawRect*(self: NSButton, rect: NSRect) =
-    discard rect
-    if self.isNil:
-      return
-    self.syncButtonCellFromView()
-    let selected = (self.NSControl).cell()
-    if selected.isNil:
-      return
-    selected.setControlView(self.NSView)
-    selected.drawWithFrame(self.bounds(), self.NSView)
-
   method setTitleWithMnemonic*(self: NSButton, value: NSString) =
-    self.setTitle(stripMnemonicMarkers(value))
+    raise newException(NSException, "unimplemented")
 
+  method xSetFontFamilyName*(self: NSButton, familyName: NSString) =
+    if familyName.isNil or
+        familyName == NSNoSelectionMarker
+        familyName == NSMultipleValuesMarker
+        familyName == NSNotApplicableMarker:
+      return
+    
+    let currentFont = self.font()
+    let size = currentFont.pointSize()
+
+    let newFont = NSFont.fontWithName(familyName, size = size)
+    if newFont.notNil:
+      self.setFont(newFont)
+  
   method dealloc(self: NSButton) {.used.} =
     self.onClick = nil
     discard callSuperIdFrom(NSButton, self, getSelector("dealloc"))
