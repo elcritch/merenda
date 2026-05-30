bcd201f6d31116a2ae935469e479c4d0493af277
Jaremy Creechley <creechley@gmail.com>
2026-05-30 21:07:30 +0300

    fixup combobox

--- src/knutella/nimkit/drawing.nim
+++ src/knutella/nimkit/drawing.nim
@@ -2,15 +2,21 @@ import figdraw/fignodes
 
 import ./types as nimkitTypes
 
+const
+  DefaultDrawLevel* = 50.ZLevel
+  PopupDrawLevel* = 100.ZLevel
+
 type DrawContext* = ref object
-  xList: RenderList
+  xRenders: Renders
   xParent: FigIdx
   xLocalOriginInWindow: nimkitTypes.Point
   xBounds: nimkitTypes.Rect
   xVisibleRect: nimkitTypes.Rect
 
 proc initDrawContext*(): DrawContext =
-  DrawContext()
+  result =
+    DrawContext(xRenders: Renders(layers: initOrderedTable[ZLevel, RenderList]()))
+  result.xRenders.layers[DefaultDrawLevel] = RenderList()
 
 proc beginDraw*(
     context: DrawContext,
@@ -25,7 +31,12 @@ proc beginDraw*(
   context.xVisibleRect = visibleRect
 
 proc renderList*(context: DrawContext): RenderList =
-  context.xList
+  if DefaultDrawLevel in context.xRenders.layers:
+    return context.xRenders.layers[DefaultDrawLevel]
+  RenderList()
+
+proc renders*(context: DrawContext): Renders =
+  context.xRenders
 
 proc localRectToWindow*(
     context: DrawContext, rect: nimkitTypes.Rect
@@ -43,11 +54,16 @@ proc bounds*(context: DrawContext): nimkitTypes.Rect =
 proc visibleRect*(context: DrawContext): nimkitTypes.Rect =
   context.xVisibleRect
 
-proc addFig*(context: DrawContext, parent: FigIdx, node: Fig): FigIdx {.discardable.} =
+proc addFig*(
+    context: DrawContext, layer: ZLevel, parent: FigIdx, node: Fig
+): FigIdx {.discardable.} =
   if parent == (-1).FigIdx:
-    context.xList.addRoot(node)
+    context.xRenders.addRoot(layer, node)
   else:
-    context.xList.addChild(parent, node)
+    context.xRenders.addChild(layer, parent, node)
+
+proc addFig*(context: DrawContext, parent: FigIdx, node: Fig): FigIdx {.discardable.} =
+  context.addFig(DefaultDrawLevel, parent, node)
 
 proc addFig*(context: DrawContext, node: Fig): FigIdx {.discardable.} =
   context.addFig(context.xParent, node)
