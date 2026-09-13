acbd99959444e1d191b84b74c3be660e827cefda
Jaremy Creechley <creechley@gmail.com>
2026-09-13 22:54:33 +0300

    Use UTF-8 glyph operators through native facade

--- src/merenda/nimkit/text/textviews.nim
+++ src/merenda/nimkit/text/textviews.nim
@@ -1,7 +1,7 @@
 import std/[hashes, math, options, strutils, unicode]
 
 when defined(useNativeDynlib):
-  from figdraw/dynlib import GlyphArrangement, lineGlyphRanges
+  from figdraw/dynlib import GlyphArrangement, lineGlyphRanges, len, `[]`
 else:
   from figdraw import GlyphArrangement, lineGlyphRanges, len, `[]`
 
